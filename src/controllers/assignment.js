1de7ac4eb42d60bc990eaf947841eeef96e0b253
nhatnguyenxd <nguyennhat2504.nn@gmail.com>
2022-10-10 09:50:25 +0700

    fix

--- src/controllers/assignment.js
+++ src/controllers/assignment.js
@@ -1,18 +1,23 @@
-<<<<<<< HEAD
-const { assignmentService } = require('../services/index')
+const { assignmentService, questionService } = require('../services/index')
 
 module.exports = {
   //load assignments
   getAssignmentsOfTeacher: async (req, res) => {
-    let teacherId = req.params.teacherId
-    console.log(req.params.teacherId)
-    let assignments = await assignmentService.findAssignmentsByTeacherId(
+    const teacherId = req.params.teacherId
+    const assignments = await assignmentService.findAssignmentsByTeacherId(
       teacherId,
     )
     return res.send(assignments)
   },
+  getAssignmentWithQuestion: async (req, res) => {
+    const id = req.params.id
+    const assignment = await assignmentService.findAssignment(id)
+    assignment.questions = await questionService.findQuestionByAssignmentId(id)
+    console.log(assignment)
+    return res.send(assignment)
+  },
   postAssignment: async (req, res) => {
-    let assignment = {
+    const assignment = {
       assignmentName: req.body.assignmentName,
       dateDue: req.body.dateDue || new Date(),
       time: req.body.time || 0,
@@ -21,68 +26,30 @@ module.exports = {
       teacherId: req.body.teacherId,
       isDeleted: 0,
     }
-    let assignmentNew = await assignmentService.createAssignment(assignment)
+    const assignmentNew = await assignmentService.createAssignment(assignment)
+    if (!assignmentNew)
+      return res.json({ error: 'Could not create assignment' })
     return res.send(assignmentNew)
   },
+  putAssignment: async (req, res) => {
+    const id = req.params.id
+    const assignment = {
+      assignmentName: req.body.assignmentName,
+      dateDue: req.body.dateDue,
+      time: req.body.time,
+      totalScore: req.body.totalScore,
+      redo: req.body.redo,
+      teacherId: req.body.teacherId,
+    }
+    const assignmentUpdated = await assignmentService.updateAssignment(
+      id,
+      assignment,
+    )
+    return res.send(assignmentUpdated)
+  },
+  deleteAssignment: async (req, res) => {
+    const id = req.params.id
+    const assignmentDeleted = await assignmentService.deleteAssignment(id)
+    return res.send(assignmentDeleted)
+  },
 }
-=======
-const { assignmentService, questionService } = require('../services/index');
-
-module.exports = {
-    //load assignments
-    getAssignmentsOfTeacher: async (req, res) => {
-        const teacherId = req.params.teacherId;
-        const assignments = await assignmentService.findAssignmentsByTeacherId(
-            teacherId
-        );
-        return res.send(assignments);
-    },
-    getAssignmentWithQuestion: async (req, res) => {
-        const id = req.params.id;
-        const assignment = await assignmentService.findAssignment(id);
-        assignment.questions = await questionService.findQuestionByAssignmentId(
-            id
-        );
-        console.log(assignment);
-        return res.send(assignment);
-    },
-    postAssignment: async (req, res) => {
-        const assignment = {
-            assignmentName: req.body.assignmentName,
-            dateDue: req.body.dateDue || new Date(),
-            time: req.body.time || 0,
-            totalScore: req.body.totalScore || 100,
-            redo: req.body.redo || 0,
-            teacherId: req.body.teacherId,
-            isDeleted: 0,
-        };
-        const assignmentNew = await assignmentService.createAssignment(
-            assignment
-        );
-        if (!assignmentNew)
-            return res.json({ error: 'Could not create assignment' });
-        return res.send(assignmentNew);
-    },
-    putAssignment: async (req, res) => {
-        const id = req.params.id;
-        const assignment = {
-            assignmentName: req.body.assignmentName,
-            dateDue: req.body.dateDue,
-            time: req.body.time,
-            totalScore: req.body.totalScore,
-            redo: req.body.redo,
-            teacherId: req.body.teacherId,
-        };
-        const assignmentUpdated = await assignmentService.updateAssignment(
-            id,
-            assignment
-        );
-        return res.send(assignmentUpdated);
-    },
-    deleteAssignment: async (req, res) => {
-        const id = req.params.id;
-        const assignmentDeleted = await assignmentService.deleteAssignment(id);
-        return res.send(assignmentDeleted);
-    },
-};
->>>>>>> af3f655abcf1fa2e5011b1516446ee6fd264cbc0
--- src/controllers/question.js
+++ src/controllers/question.js
@@ -1,38 +1,6 @@
 const { questionService, skillQuestionService } = require('../services/index')
 
 module.exports = {
-<<<<<<< HEAD
-  postQuestion: async (req, res) => {
-    const question = {
-      content: req.body.content,
-      image: req.body.image || '',
-      option: req.body.option && req.body.option.toString(),
-      result: req.body.result,
-      hint: req.body.hint,
-      score: req.body.score,
-      questionTypeId: req.body.questionTypeId,
-      assignmentId: req.body.assignmentId,
-      isDeleted: 0,
-    }
-    let questionNew = await questionService.createQuestion(question)
-
-    const skillIds = req.body.skillIds
-
-    const listSkillQuestion = new Array()
-    for (let i = 0; i < skillIds.length; ++i) {
-      listSkillQuestion.push({
-        questionId: questionNew.id,
-        skillId: skillIds[i],
-        isDeleted: 0,
-      })
-    }
-    const skillQuestion = await skillQuestionService.createSkillQuestion(
-      listSkillQuestion,
-    )
-
-    questionNew.option = questionNew.option.split(',')
-    return res.send(questionNew)
-  },
   getQuestionOfAssignment: async (req, res) => {
     let assignmentId = req.params.assignmentId
     let questions = await questionService.findQuestionByAssignmentId(
@@ -45,89 +13,66 @@ module.exports = {
     }
     return res.send(questions)
   },
+  postQuestion: async (req, res) => {
+    const question = {
+      content: req.body.content,
+      image: req.body.image || '',
+      option: req.body.option && JSON.stringify(req.body.option),
+      result: req.body.result || '',
+      hint: req.body.hint || '',
+      score: req.body.score,
+      questionTypeId: req.body.questionTypeId,
+      assignmentId: req.body.assignmentId,
+      isDeleted: 0,
+    }
+    let questionNew = await questionService.createQuestion(question)
+
+    const skillIds = req.body.skillIds || new Array()
+
+    const listSkillQuestion = new Array()
+    for (let i = 0; i < skillIds.length; ++i) {
+      listSkillQuestion.push({
+        questionId: questionNew.id,
+        skillId: skillIds[i],
+        isDeleted: 0,
+      })
+    }
+    await skillQuestionService.createSkillQuestion(listSkillQuestion)
+    questionNew.option = JSON.parse(questionNew.option)
+    return res.json(questionNew)
+  },
+  putQuestion: async (req, res) => {
+    const id = req.params.id
+    console.log(req.body.option)
+    const question = {
+      content: req.body.content,
+      image: req.body.image || '',
+      option: req.body.option && JSON.stringify(req.body.option),
+      result: req.body.result || '',
+      hint: req.body.hint || '',
+      score: req.body.score,
+      questionTypeId: req.body.questionTypeId,
+      assignmentId: req.body.assignmentId,
+    }
+    let questionUpdated = await questionService.updateQuestion(id, question)
+
+    const skillIds = req.body.skillIds || new Array()
+    const listSkillQuestion = new Array()
+    for (let i = 0; i < skillIds.length; ++i) {
+      listSkillQuestion.push({
+        questionId: questionUpdated.id,
+        skillId: skillIds[i],
+        isDeleted: 0,
+      })
+    }
+    await skillQuestionService.createSkillQuestion(listSkillQuestion)
+    if (questionUpdated.option)
+      questionUpdated.option = JSON.parse(questionUpdated.option)
+    return res.json(questionUpdated)
+  },
   deleteQuestion: async (req, res) => {
     let id = req.params.id
     let questionDeleted = await questionService.deleteQuestion(id)
-    return res.send(questionDeleted)
+    return res.json(questionDeleted)
   },
 }
-=======
-    getQuestionOfAssignment: async (req, res) => {
-        let assignmentId = req.params.assignmentId;
-        let questions = await questionService.findQuestionByAssignmentId(
-            assignmentId
-        );
-
-        for (let i = 0; i < questions.length; i++) {
-            questions[i].option =
-                questions[i].option && questions[i].option.split(',');
-        }
-        return res.send(questions);
-    },
-    postQuestion: async (req, res) => {
-        const question = {
-            content: req.body.content,
-            image: req.body.image || '',
-            option: req.body.option && JSON.stringify(req.body.option),
-            result: req.body.result || '',
-            hint: req.body.hint || '',
-            score: req.body.score,
-            questionTypeId: req.body.questionTypeId,
-            assignmentId: req.body.assignmentId,
-            isDeleted: 0,
-        };
-        let questionNew = await questionService.createQuestion(question);
-
-        const skillIds = req.body.skillIds || new Array();
-
-        const listSkillQuestion = new Array();
-        for (let i = 0; i < skillIds.length; ++i) {
-            listSkillQuestion.push({
-                questionId: questionNew.id,
-                skillId: skillIds[i],
-                isDeleted: 0,
-            });
-        }
-        await skillQuestionService.createSkillQuestion(listSkillQuestion);
-        questionNew.option = JSON.parse(questionNew.option);
-        return res.json(questionNew);
-    },
-    putQuestion: async (req, res) => {
-        const id = req.params.id;
-        console.log(req.body.option);
-        const question = {
-            content: req.body.content,
-            image: req.body.image || '',
-            option: req.body.option && JSON.stringify(req.body.option),
-            result: req.body.result || '',
-            hint: req.body.hint || '',
-            score: req.body.score,
-            questionTypeId: req.body.questionTypeId,
-            assignmentId: req.body.assignmentId,
-        };
-        let questionUpdated = await questionService.updateQuestion(
-            id,
-            question
-        );
-
-        const skillIds = req.body.skillIds || new Array();
-        const listSkillQuestion = new Array();
-        for (let i = 0; i < skillIds.length; ++i) {
-            listSkillQuestion.push({
-                questionId: questionUpdated.id,
-                skillId: skillIds[i],
-                isDeleted: 0,
-            });
-        }
-        await skillQuestionService.createSkillQuestion(listSkillQuestion);
-        if (questionUpdated.option)
-            questionUpdated.option = JSON.parse(questionUpdated.option);
-        return res.json(questionUpdated);
-    },
-    deleteQuestion: async (req, res) => {
-        let id = req.params.id;
-        let questionDeleted = await questionService.deleteQuestion(id);
-        return res.json(questionDeleted);
-    },
-};
->>>>>>> af3f655abcf1fa2e5011b1516446ee6fd264cbc0
