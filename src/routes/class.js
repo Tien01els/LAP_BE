1d181c9509639527f0dbc41a97d508a099b1cb55
Nhat Nguyen <nhatnm@primascx.net>
2022-10-15 13:43:01 +0700

    add funcs

--- src/routes/class.js
+++ src/routes/class.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const { classController } = require('../controllers/index');
+const express = require('express')
+const { classController } = require('../controllers/index')
 
-const classRouter = express.Router();
+const classRouter = express.Router()
 
 classRouter.get('/:teacherId', classController.getClasses)
 classRouter.get(
-    '/:teacherId/:gradeId',
-    classController.getClassesByTeacherIdAndGradeId
-);
+  '/:teacherId/:gradeId',
+  classController.getClassesByTeacherIdAndGradeId,
+)
 
-module.exports = classRouter;
+module.exports = classRouter
